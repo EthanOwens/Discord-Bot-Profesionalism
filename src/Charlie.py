be186ae1154362a7106d62535b4bc52fdd99e13a
Ethan Owens <owensitf3@gmail.com>
2021-10-29 10:14:25 -0500

    Added sqlite3 table

--- src/Charlie.py
+++ src/Charlie.py
@@ -1,12 +1,24 @@
 import discord
 import os
 from dotenv import load_dotenv
+import sqlite3
+
+con = sqlite3.connect('charlie.db')
+cur = con.cursor()
+
+if(not cur.execute('SELECT name from sqlite_master WHERE type = \'table\' AND name = \'{player_data}\'')):
+    cur.execute('CREATE TABLE player_data (id, name, guild, balance, exp)') #create table
+    con.commit()
+    print("Table created")
+else:
+    print("Table player_data already exists")
 
 #loads in our Token from our .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents = intents)
 
 #Basic python function
 def contains(arr, item):
@@ -22,6 +34,10 @@ async def on_ready():
     for guild in client.guilds:
         print(f'Charlie is connected to {guild.name}')
 
+@client.event
+async def on_guild_join(guild):
+    for member in guild.members:
+        print(f'Member: {member}')
 
 #on_message will check every message sent and trigger when a condition is met
 @client.event
@@ -30,5 +46,6 @@ async def on_message(message):
         #sending messages require you to include "await" to wait for the bot to be done with any existing processes before running your code
         await message.channel.send("I'm a happy dino!")
 
+cur.close()
 #running the bot using our unique bot Token
 client.run(TOKEN)
--- src/CharlieCommands.py
+++ src/CharlieCommands.py
@@ -2,21 +2,39 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+import sqlite3
+
+con = sqlite3.connect('charlie.db')
+cur = con.cursor()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix=">") #set command keyfigure to >
 
-def toID(mention):
-    return str(mention).replace('@', '').replace('>', '').replace('<', '').replace('!', '')  # strips a mention down to the id
+
 
 #Bot commandsd must start with @bot.command and async, similarly to the events
 #typing >help in a discord server with this bot will bring up a help list of all commands
 @bot.command(help = "Returns the id of the user")
 # ctx (context) is a discord specific parameter that is required for all discord commands. It will refer to data about the user that ran the command
-async def userID(ctx, arg):
-    arg = toID(arg)
-    await ctx.channel.send(f'User Id: {arg}')
+async def userID(ctx, user: discord.User):
+    await ctx.channel.send(f'User Id: {user.id}')
+
+@bot.command(help = "Manually insert row of data by command line: mention, starting balance. ")
+async def manualInsert(ctx, user: discord.User, arg1):
+    cur.execute(f'INSERT INTO player_data VALUES ("{user.id}", "{user.name}", "{ctx.guild}", {arg1}, {0})')
+    con.commit()
+    print(f'Tuple inserted into player_data: ({user.id}, {user.name}, {ctx.guild}, {arg1}, {0})')
+    cur.close()
+
+@bot.command(help = "Returns balance of user")
+async def bal(ctx):
+    cur.execute(f'SELECT balance FROM player_data WHERE id = "{ctx.author.id}"')
+    await ctx.channel.send(f'Your balance is {cur.fetchone()[0]}')
+
+@bot.command(help = "Returns guild name")
+async def guild(ctx):
+    await ctx.channel.send(f'Guild: {ctx.guild}')
 
 bot.run(TOKEN)
