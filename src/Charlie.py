122a1788bf6ebb507aef4d07b661ff7c8a70e2c4
Ethan Owens <owensitf3@gmail.com>
2021-10-31 18:16:49 -0500

    Finished up the program. Added level system with exp gained per comment

--- src/Charlie.py
+++ src/Charlie.py
@@ -1,14 +1,14 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
 import sqlite3
-import re
 
 con = sqlite3.connect('charlie.db')
 cur = con.cursor()
 
-if(not cur.execute('SELECT name from sqlite_master WHERE type = \'table\' AND name = \'{player_data}\'')):
-    cur.execute('CREATE TABLE player_data (id, name, guild, balance, exp)') #create table
+if(not cur.execute('SELECT name FROM sqlite_master WHERE type = \'table\' AND name = \'{player_data}\'')):
+    cur.execute('CREATE TABLE player_data (id varchar(50), name varchar(50), guild varchar(50), balance int, exp int, level int)') #create table
     con.commit()
     print("Table created")
 else:
@@ -20,12 +20,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.all()
 client = discord.Client(intents = intents)
-
-#Basic python function
-def contains(arr, item):
-    for i in range(len(arr)):
-        if arr[i].lower() == item:
-            return True
 
 #Discord events must be started with @client.event
 #Discord event commands and events are couruntine functions and must start with async
@@ -40,9 +34,9 @@ async def on_ready():
             if(len(data) == 0):
                 #add new member to the database with starting balance of 50
                 print(f'Adding {member.name} to player_data')
-                cur.execute(f'INSERT INTO player_data VALUES ("{member.id}", "{member.name}", "{guild}", {50}, {0})')
+                cur.execute(f'INSERT INTO player_data VALUES ("{member.id}", "{member.name}", "{guild}", 50, 0)')
             else:
-                print("user already in database")
+                print(f'{member.name} already in database')
     con.commit()
 
 @client.event
@@ -57,6 +51,24 @@ async def on_message(message):
         #sending messages require you to include "await" to wait for the bot to be done with any existing processes before running your code
         await message.channel.send("I'm a happy dino!")
 
-    msg_length = re.sub(":.:")
+    if(not message.author.bot):
+        cur.execute(f'SELECT exp, level FROM player_data WHERE id = "{message.author.id}"')
+        row = cur.fetchone()
+        exp_gain = 15 + len(message.content.replace(" ", ""))
+        new_exp = int(row[0]) + exp_gain
+        cur_level = row[1]
+        if(new_exp/500 >= cur_level):
+            new_level = int(new_exp/500) + 1
+            bal_increase = random.randint(15, 100)
+
+            cur.execute(f'UPDATE player_data SET level = {new_level} WHERE id = "{message.author.id}"')
+            con.commit()
+            cur.execute(f'UPDATE player_data SET balance = {bal_increase} WHERE id = "{message.author.id}"')
+            cur.commit()
+            await message.channel.send(f'You leveled up! Your new level is {new_level}\n You also found a little bundle of money! You are now {bal_increase} richer.')
+
+        cur.execute(f'UPDATE player_data SET exp = {new_exp} WHERE id = "{message.author.id}"')
+        con.commit()
+
 #running the bot using our unique bot Token
 client.run(TOKEN)
--- src/CharlieCommands.py
+++ src/CharlieCommands.py
@@ -12,27 +12,45 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix=">") #set command keyfigure to >
 
-cur.execute("ALTER TABLE player_table ADD level INT DEFAULT 0 AFTER exp")
-
 #Bot commandsd must start with @bot.command and async, similarly to the events
 #typing >help in a discord server with this bot will bring up a help list of all commands
 # ctx (context) is a discord specific parameter that is required for all discord commands. It will refer to data about the user that ran the command
 
 @bot.command(help = "Manually insert row of data by command line: mention, starting balance. ")
 async def manualInsert(ctx, user: discord.User, arg1):
-    cur.execute(f'INSERT INTO player_data VALUES ("{user.id}", "{user.name}", "{ctx.guild}", {arg1}, {0})')
+    cur.execute(f'INSERT INTO player_data VALUES ("{user.id}", "{user.name}", "{ctx.guild}", {arg1}, 0, 1)')
     con.commit()
-    print(f'Tuple inserted into player_data: ({user.id}, {user.name}, {ctx.guild}, {arg1}, {0})')
+    print(f'Tuple inserted into player_data: ({user.id}, {user.name}, {ctx.guild}, {arg1}, 0, 1)')
 
 @bot.command(help = "Returns the balance of mentioned user")
 async def bal(ctx, user: discord.User):
     cur.execute(f'SELECT balance FROM player_data WHERE id = "{user.id}"')
     await ctx.channel.send(f'{user.name}, your balance is {cur.fetchone()[0]}')
 
-@bot.command()
-async def level(ctx, user:discord.User):
-    cur.execute(f'Select level FROM player_table WHERE id = "{user.id}"')
+@bot.command(help = "Shows general data on a user")
+async def profile(ctx, user: discord.User):
+    cur.execute(f'SELECT balance, exp, level FROM player_data WHERE id = "{user.id}"')
     #add an embed here to return the data with their icon and level
+    row = cur.fetchone()
+    pfp = user.avatar_url
+    profile = discord.Embed(title = f'{user.name}\'s profile', description = f'Balance: {row[0]}\n'
+                                                                             f'Current Experience: {row[1]}\n '
+                                                                             f'Level: {row[2]}')
+    profile.set_thumbnail(url=(pfp))
+    await ctx.channel.send(embed = profile)
+
+@bot.command(help = "Pay user x amount")
+async def pay(ctx, user: discord.User, amount):
+    cur.execute(f'SELECT balance FROM rat_bank.users WHERE user_id = {ctx.author.id}')
+    payer_bal = int(cur.fetchone()[0])
+
+    if(payer_bal > amount):
+        cur.execute(f'UPDATE rat_bank.users SET balance = ({amount} + balance) WHERE user_id = "{user.id}"')
+        cur.execute(f'UPDATE rat_bank.users SET balance = (balance - {amount}) WHERE user_id = "{ctx.author.id}"')
+        con.commit()
+        await ctx.channel.send(f'{user.name}, you have been payed {amount} by {ctx.author.id}')
+    else:
+        await ctx.channel.send(f'{ctx.author.name}, you have insufficient funds')
 
 @bot.command(help = "Returns all rows to terminal")
 async def printRows(ctx):
@@ -40,10 +58,5 @@ async def printRows(ctx):
     await ctx.channel.send("Printing player data to terminal")
     for row in cur:
         print(row)
-
-@bot.command()
-async def printID(ctx, user: discord.User):
-    cur.execute(f'SELECT id FROM player_data WHERE id = "{user.id}"')
-    print(cur.fetchone()[0])
 
 bot.run(TOKEN)
